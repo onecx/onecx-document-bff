de7b8298e5597970de934f0531b5a88e0dcd2e50
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2026-05-04 12:36:41 +0200

    feat: aligned api to updated mimetype handling

--- src/test/java/org/tkit/onecx/document/bff/controllers/SupportedMimeTypeControllerTest.java
+++ src/test/java/org/tkit/onecx/document/bff/controllers/SupportedMimeTypeControllerTest.java
@@ -17,9 +17,6 @@ import org.mockserver.client.MockServerClient;
 import org.mockserver.model.JsonBody;
 import org.tkit.onecx.document.bff.AbstractTest;
 
-import gen.org.tkit.onecx.document.client.model.SupportedMimeType;
-import gen.org.tkit.onecx.document.rs.internal.model.SupportedMimeTypeCreateUpdateDTO;
-import gen.org.tkit.onecx.document.rs.internal.model.SupportedMimeTypeDTO;
 import io.quarkiverse.mockserver.test.InjectMockServerClient;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
@@ -29,11 +26,7 @@ import io.restassured.http.ContentType;
 @TestHTTPEndpoint(SupportedMimeTypeController.class)
 class SupportedMimeTypeControllerTest extends AbstractTest {
 
-    private static final String MIME_ID = "test-mime-id";
-    private static final String USERNAME_TOKEN = "apm-username";
     private static final String SVC_MOCK_ID = "DOC_MGMT_SVC_MOCK";
-    private static final String SEC_SVC_MOCK_ID = "SEC_DOC_MGMT_SVC_MOCK";
-    private static final String FILE_STORAGE_MOCK_ID = "FILE_STORAGE_SVC_MOCK";
 
     @InjectMockServerClient
     MockServerClient mockServerClient;
@@ -42,8 +35,6 @@ class SupportedMimeTypeControllerTest extends AbstractTest {
     public void resetExpectation() {
         try {
             mockServerClient.clear(SVC_MOCK_ID);
-            mockServerClient.clear(FILE_STORAGE_MOCK_ID);
-            mockServerClient.clear(SEC_SVC_MOCK_ID);
         } catch (Exception _) {
             // mockid not existing
         }
@@ -52,10 +43,7 @@ class SupportedMimeTypeControllerTest extends AbstractTest {
     @Test
     @DisplayName("GET / - should return all supported mime types")
     void getAllSupportedMimeTypes_shouldReturnMimeTypes_whenServiceRespondsOk() throws Exception {
-        var mimeType = new SupportedMimeType();
-        mimeType.setId(MIME_ID);
-        mimeType.setName("application/pdf");
-        mimeType.setDescription("PDF Document");
+        var mimeType = "image/png";
 
         mockServerClient
                 .when(request()
@@ -76,136 +64,8 @@ class SupportedMimeTypeControllerTest extends AbstractTest {
                 .then()
                 .statusCode(Response.Status.OK.getStatusCode())
                 .extract()
-                .body().as(SupportedMimeTypeDTO[].class);
+                .body().as(String[].class);
 
-        assertThat(response[0].getId()).isEqualTo(MIME_ID);
-        assertThat(response[0].getName()).isEqualTo("application/pdf");
-    }
-
-    @Test
-    @DisplayName("POST / - should create supported mime type")
-    void createSupportedMimeType_shouldReturnCreatedMimeType_whenRequestIsValid() throws Exception {
-        var requestDto = new SupportedMimeTypeCreateUpdateDTO();
-        requestDto.setName("application/pdf");
-        requestDto.setDescription("PDF Document");
-
-        var mimeType = new SupportedMimeType();
-        mimeType.setId(MIME_ID);
-        mimeType.setName("application/pdf");
-        mimeType.setDescription("PDF Document");
-
-        mockServerClient
-                .when(request()
-                        .withMethod("POST")
-                        .withPath("/internal/supported-mime-type"))
-                .withId(SVC_MOCK_ID)
-                .respond(response()
-                        .withStatusCode(Response.Status.CREATED.getStatusCode())
-                        .withContentType(org.mockserver.model.MediaType.APPLICATION_JSON)
-                        .withBody(JsonBody.json(mimeType)));
-
-        var response = given()
-                .when()
-                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
-                .header(APM_HEADER_PARAM, ADMIN)
-                .contentType(ContentType.JSON)
-                .body(requestDto)
-                .post()
-                .then()
-                .statusCode(Response.Status.CREATED.getStatusCode())
-                .extract()
-                .body().as(SupportedMimeTypeDTO.class);
-
-        assertThat(response.getId()).isEqualTo(MIME_ID);
-    }
-
-    @Test
-    @DisplayName("GET /{id} - should return supported mime type by id")
-    void getSupportedMimeTypeById_shouldReturnMimeType_whenServiceRespondsOk() throws Exception {
-        var mimeType = new SupportedMimeType();
-        mimeType.setId(MIME_ID);
-        mimeType.setName("application/pdf");
-        mimeType.setDescription("PDF Document");
-
-        mockServerClient
-                .when(request()
-                        .withMethod("GET")
-                        .withPath("/internal/supported-mime-type/" + MIME_ID))
-                .withId(SVC_MOCK_ID)
-                .respond(response()
-                        .withStatusCode(Response.Status.OK.getStatusCode())
-                        .withContentType(org.mockserver.model.MediaType.APPLICATION_JSON)
-                        .withBody(JsonBody.json(mimeType)));
-
-        var response = given()
-                .when()
-                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
-                .header(APM_HEADER_PARAM, ADMIN)
-                .contentType(ContentType.JSON)
-                .get("/{id}", MIME_ID)
-                .then()
-                .statusCode(Response.Status.OK.getStatusCode())
-                .extract()
-                .body().as(SupportedMimeTypeDTO.class);
-
-        assertThat(response.getId()).isEqualTo(MIME_ID);
-    }
-
-    @Test
-    @DisplayName("PUT /{id} - should update supported mime type by id")
-    void updateSupportedMimeTypeById_shouldReturnUpdatedMimeType_whenRequestIsValid() throws Exception {
-        var requestDto = new SupportedMimeTypeCreateUpdateDTO();
-        requestDto.setName("application/pdf");
-        requestDto.setDescription("Portable Document Format");
-
-        var mimeType = new SupportedMimeType();
-        mimeType.setId(MIME_ID);
-        mimeType.setName("application/pdf");
-        mimeType.setDescription("Portable Document Format");
-
-        mockServerClient
-                .when(request()
-                        .withMethod("PUT")
-                        .withPath("/internal/supported-mime-type/" + MIME_ID))
-                .withId(SVC_MOCK_ID)
-                .respond(response()
-                        .withStatusCode(Response.Status.OK.getStatusCode())
-                        .withContentType(org.mockserver.model.MediaType.APPLICATION_JSON)
-                        .withBody(JsonBody.json(mimeType)));
-
-        var response = given()
-                .when()
-                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
-                .header(APM_HEADER_PARAM, ADMIN)
-                .contentType(ContentType.JSON)
-                .body(requestDto)
-                .put("/{id}", MIME_ID)
-                .then()
-                .statusCode(Response.Status.OK.getStatusCode())
-                .extract()
-                .body().as(SupportedMimeTypeDTO.class);
-
-        assertThat(response.getDescription()).isEqualTo("Portable Document Format");
-    }
-
-    @Test
-    @DisplayName("DELETE /{id} - should delete supported mime type by id")
-    void deleteSupportedMimeTypeId_shouldReturnNoContent_whenServiceRespondsNoContent() {
-        mockServerClient
-                .when(request()
-                        .withMethod("DELETE")
-                        .withPath("/internal/supported-mime-type/" + MIME_ID))
-                .withId(SVC_MOCK_ID)
-                .respond(response()
-                        .withStatusCode(Response.Status.NO_CONTENT.getStatusCode()));
-
-        given()
-                .when()
-                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
-                .header(APM_HEADER_PARAM, ADMIN)
-                .contentType(ContentType.JSON)
-                .delete("/{id}", MIME_ID)
-                .then()
-                .statusCode(Response.Status.NO_CONTENT.getStatusCode());
+        assertThat(response[0]).isEqualTo(mimeType);
     }
 }
